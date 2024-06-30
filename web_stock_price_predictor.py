c8fb2cf4cd18f4e71f3467cc21a09748007c40ab
Incharajayaram <incharajayaram2020@gmail.com>
2024-06-30 17:03:31 +0530

    Initial commit

--- web_stock_price_predictor.py
+++ web_stock_price_predictor.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from keras.models import load_model
+from keras.models import load_model # type: ignore
 import matplotlib.pyplot as plt
 import yfinance as yf
 
